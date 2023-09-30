fdb00f1e51e1e1bdd304853837879b869daed160
admin <najay@gmail.com>
2023-09-30 19:39:41 +0530

    first commit

---
+++ src/main/java/com/testcases/LoginTest.java
@@ -0,0 +1,12 @@
+package com.testcases;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class LoginTest {
+	
+	public void loginTest()
+	{
+		System.out.println("this is LoginTest");
+	}
+
+}
